d1b6feac6ba25ee6da45a64e40da3a1933b6dd70
sosinatolossa <sosinatolossa16@gmail.com>
2021-03-08 15:11:20 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlanningMyHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan Your Heist!");
+        }
+    }
+}
